0f02ba23462b3511d54591cc35a34d7c0d0ac912
Matheus Petry Pfitscher <matheusppfitscher@outlook.com>
2022-02-22 22:15:23 -0300

    Componentes criados em aula
    Co-authored-by: Solange Righi <slngvitoria@gmail.com>

--- src/components/Conteudo/Card.jsx
+++ src/components/Conteudo/Card.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const StyleCard = styled.div`
-
+    margin: 50px;
+    display: flex;
 `;
 
-export const Card = () => {
+const StyleImagem = styled.div`
+    background-image: url();
+    height: 100px;
+`;
+
+const StyleTitulo = styled.h1`
+    font-size: 14px;
+    text-transform: uppercase;
+`;
+
+const StyleTexto = styled.p`
+    font-size: 11px;
+`;
+
+export const Card = (props) => {
     return (
         <StyleCard>
-
+            <StyleImagem />
+            <StyleTitulo> {props.titulo} </StyleTitulo>
+            <StyleTexto> {props.texto} </StyleTexto>
         </StyleCard>
-    )
-}
+    );
+};
